dc7dc7b113bdf873a1002ca807472204ecc1fb19
Romain Francois <romain@thinkr.fr>
2018-02-26 11:48:13 +0100

    + the domain argument to `code`

--- R/puny.R
+++ R/puny.R
@@ -1,6 +1,7 @@
 #' punycode encoder
 #'
 #' @param s a character vector
+#' @param domain if `TRUE` the prefix `xn--` is added to the output
 #'
 #' @return s encoded in [punycode](https://en.wikipedia.org/wiki/Punycode)
 #'
@@ -18,7 +19,11 @@
 #'
 #' @importFrom stringi stri_enc_toutf32 stri_enc_toutf8
 #' @export
-code <- function(s){
+code <- function(s, domain = FALSE){
   data <- stri_enc_toutf32(stri_enc_toutf8(s))
-  .Call( puny_encode, data )
+  res <- .Call( puny_encode, data )
+  if( isTRUE(domain) ){
+    res <- paste0("xn--", res)
+  }
+  res
 }
